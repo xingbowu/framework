6d409b8159531cb4dd4f29f1880224af47c0ddda
兴博 <xingbo.wxb@alibaba-inc.com>
2017-05-12 16:32:27 +0800

    refresh

--- tsindex/src/main/java/com/example/memindex/Const.java
+++
@@ -1,9 +0,0 @@
-package com.example.memindex;
-
-/**
- * Created by xingbowu on 17/5/2.
- */
-public class Const {
-    public final static String TAGCONNECTOR ="=";
-    public final static String TAGSCONNECTOR ="&";
-}
